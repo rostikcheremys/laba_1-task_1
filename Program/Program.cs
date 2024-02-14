0897a5b47d02ddac2514df48449a722530304f88
rostikcheremys <rostikcheremys@gmail.com>
2024-02-14 21:57:06 +0200

    Initial commit

---
+++ Program/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading;
+
+namespace Program
+{
+    public delegate void MyDelegate(string text, int t);
+
+    static class Timer
+    {
+        public static void Method(string text, int t)
+        {
+            new Thread(() =>
+            {
+                while (true)
+                {
+                    Console.WriteLine(text);
+                    Thread.Sleep(TimeSpan.FromSeconds(t));
+                }
+            }).Start();
+        }
+    }
+    
+    internal abstract class Program
+    {
+        static void Main()
+        {
+            MyDelegate myDelegate = Timer.Method;
+            
+            myDelegate.Invoke("RED", 1);
+            myDelegate.Invoke("YELLOW", 5);
+        }
+    }
+}
